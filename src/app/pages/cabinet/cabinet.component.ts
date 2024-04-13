e7505b85fd8c5acd05b2db70a43430e8de587a89
alinavovkov <alinvovkov@gmail.com>
2024-04-13 20:08:39 +0300

    the user account was made

--- src/app/pages/cabinet/cabinet.component.ts
+++ src/app/pages/cabinet/cabinet.component.ts
@@ -8,6 +8,9 @@ import {AccountService} from "../../shared/services/account/account.service";
   styleUrl: './cabinet.component.scss'
 })
 export class CabinetComponent implements OnInit {
+  personalDataClicked: boolean = false;
+  orderHistoryClicked: boolean = false;
+  changePasswordClicked: boolean = false;
   constructor(
     private router: Router,
     private accountService: AccountService
@@ -15,7 +18,26 @@ export class CabinetComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  // Method to handle click event for personal data button
+  handlePersonalDataClick() {
+    this.personalDataClicked = true;
+    this.orderHistoryClicked = false;
+    this.changePasswordClicked = false;
+  }
 
+  // Method to handle click event for order history button
+  handleOrderHistoryClick() {
+    this.personalDataClicked = false;
+    this.orderHistoryClicked = true;
+    this.changePasswordClicked = false;
+  }
+
+  // Method to handle click event for change password button
+  handleChangePasswordClick() {
+    this.personalDataClicked = false;
+    this.orderHistoryClicked = false;
+    this.changePasswordClicked = true;
+  }
   logOut(): void {
     this.router.navigate(['/']);
     localStorage.removeItem('currentUser');
--- src/app/pages/cabinet/personal-data/personal-data.component.ts
+++ src/app/pages/cabinet/personal-data/personal-data.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AccountService} from "../../../shared/services/account/account.service";
 
 @Component({
   selector: 'app-personal-data',
   templateUrl: './personal-data.component.html',
   styleUrl: './personal-data.component.scss'
 })
-export class PersonalDataComponent {
+export class PersonalDataComponent implements OnInit{
+  constructor(
+    private router: Router,
+    private accountService: AccountService
+  ) { }
 
+  ngOnInit(): void {
+  }
+  logOut(): void {
+    this.router.navigate(['/']);
+    localStorage.removeItem('currentUser');
+    this.accountService.isUserLogin$.next(true);
+  }
 }
